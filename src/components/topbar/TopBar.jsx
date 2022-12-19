6e49ed07119c2d0271518e593b5dfb305df844cb
Iryna Shumovska <shumovska_ia@groupbwt.com>
2022-12-19 21:06:48 +0200

    add TopBar

---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,43 @@
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import "./topbar.css";
+
+export default function TopBar(props) {
+    
+    return (
+        <div className="topbarContainer">
+            <div className="topbarLeft">
+                <span className="logo">PandaChat</span>
+            </div>
+            <div className="topbarCenter">
+                <div className="searchbar">
+                    <SearchIcon/>
+                    <input placeholder="Search for friends, posts or videos" className="searchInput" />
+                </div>
+            </div>
+            <div className="topbarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <PersonIcon/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <ChatIcon/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                    <div className="topbarIconItem">
+                        <NotificationsIcon/>
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+                <img src="/access/person/1.jpeg" alt="Panda" className="topbarImg" />
+            </div>
+        </div>
+    )
+}
