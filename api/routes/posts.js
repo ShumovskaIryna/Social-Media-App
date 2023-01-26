4d1f7da442ed21de72ba536388ca06f4f58ddaad
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-01-27 01:27:39 +0200

    install all for API

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -13,6 +13,7 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //update a post
 
 router.put("/:id", async (req, res) => {
@@ -28,6 +29,7 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //delete a post
 
 router.delete("/:id", async (req, res) => {
@@ -43,6 +45,7 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //like / dislike a post
 
 router.put("/:id/like", async (req, res) => {
@@ -59,6 +62,7 @@ router.put("/:id/like", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //get a post
 
 router.get("/:id", async (req, res) => {
