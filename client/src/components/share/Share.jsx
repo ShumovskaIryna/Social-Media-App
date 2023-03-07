57b2f7c713b6e0d767318b389524a1a99f599227
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-03-07 16:48:23 +0200

    Share post with upload photo

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -21,8 +21,19 @@ export default function Share() {
       userId: user._id,
       desc: desc.current.value,
     };
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      newPost.img = fileName;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
     try {
       await axios.post("/posts", newPost);
+      window.location.reload()
     } catch (err) {}
   };
 
